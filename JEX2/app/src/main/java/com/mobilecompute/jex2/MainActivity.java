60609653b9808f8e7a629803fcbac4e0e3366d4f
stories2 <stories2@naver.com>
2017-09-23 21:24:13 +0900

    JEX2 module : refact event to function

--- JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
+++ JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
@@ -24,6 +24,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         InitLayout();
+        EventLayout();
+    }
+
+    public void EventLayout() {
 
         checkEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
