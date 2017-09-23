89e928df025cfa69d826efd540dc9c0fcb8cc70e
stories2 <stories2@naver.com>
2017-09-23 21:00:41 +0900

    JEX2 module : init layout items

--- JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
+++ JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
@@ -2,12 +2,27 @@ package com.mobilecompute.jex2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
+
+    CheckBox checkEnable, checkClickable, checkRotate;
+    Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitLayout();
+    }
+
+    public void InitLayout() {
+        checkEnable = (CheckBox) findViewById(R.id.checkEnable);
+        checkClickable = (CheckBox) findViewById(R.id.checkClickable);
+        checkRotate = (CheckBox) findViewById(R.id.checkRotate);
+
+        btnTest = (Button) findViewById(R.id.btnTest);
     }
 }
