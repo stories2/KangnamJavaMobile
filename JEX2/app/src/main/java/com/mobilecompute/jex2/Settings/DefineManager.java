f60c71f4fd2ac4dc21fa0563c20fc75a6f5f8e96
stories2 <stories2@naver.com>
2017-09-23 21:02:51 +0900

    JEX2 module : make logging utils

---
+++ JEX2/app/src/main/java/com/mobilecompute/jex2/Settings/DefineManager.java
@@ -0,0 +1,17 @@
+package com.mobilecompute.jex2.Settings;
+
+/**
+ * Created by stories2 on 2017. 9. 23..
+ */
+
+public class DefineManager {
+    public static final int
+            LOG_LEVEL_VERBOSE = 0,
+            LOG_LEVEL_DEBUG = 1,
+            LOG_LEVEL_INFO = 2,
+            LOG_LEVEL_WARN = 3,
+            LOG_LEVEL_ERROR = 4;
+
+    public static final String
+            APP_NAME = "연습문제 4-7_김현우 201501180";
+}
