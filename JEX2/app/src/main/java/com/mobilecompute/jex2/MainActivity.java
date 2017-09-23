fd8565d335d27b21e8dc95e2689b1ea451d9da1c
stories2 <stories2@naver.com>
2017-09-23 21:08:31 +0900

    JEX2 module : can get all the layout items event

--- JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
+++ JEX2/app/src/main/java/com/mobilecompute/jex2/MainActivity.java
@@ -2,8 +2,13 @@ package com.mobilecompute.jex2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import static com.mobilecompute.jex2.Settings.DefineManager.LOG_LEVEL_INFO;
+import static com.mobilecompute.jex2.Utils.LogManager.PrintLog;
 
 public class MainActivity extends Activity {
 
@@ -16,13 +21,46 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         InitLayout();
+
+        checkEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean enable) {
+                PrintLog("MainActivity", "onCreate/onCheckedChanged", "enable event accepted: " + enable, LOG_LEVEL_INFO);
+            }
+        });
+
+        checkClickable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean enable) {
+                PrintLog("MainActivity", "onCreate/onCheckedChanged", "clickable event accepted: " + enable, LOG_LEVEL_INFO);
+            }
+        });
+
+        checkRotate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean enable) {
+                PrintLog("MainActivity", "onCreate/onCheckedChanged", "rotate event accepted: " + enable, LOG_LEVEL_INFO);
+            }
+        });
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PrintLog("MainActivity", "onCreate/onClick", "test button clicked", LOG_LEVEL_INFO);
+            }
+        });
     }
 
     public void InitLayout() {
+
+        PrintLog("MainActivity", "InitLayout", "init layout items", LOG_LEVEL_INFO);
+
         checkEnable = (CheckBox) findViewById(R.id.checkEnable);
         checkClickable = (CheckBox) findViewById(R.id.checkClickable);
         checkRotate = (CheckBox) findViewById(R.id.checkRotate);
 
         btnTest = (Button) findViewById(R.id.btnTest);
+
+        PrintLog("MainActivity", "InitLayout", "init ok", LOG_LEVEL_INFO);
     }
 }
