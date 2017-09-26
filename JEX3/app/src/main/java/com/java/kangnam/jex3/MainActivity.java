a72374ef3b308a8617776798e62c11af7e1176ef
Kim HyunWoo <stories2@naver.com>
2017-09-26 17:06:28 +0900

    JEX3 module : make calculate

--- JEX3/app/src/main/java/com/java/kangnam/jex3/MainActivity.java
+++ JEX3/app/src/main/java/com/java/kangnam/jex3/MainActivity.java
@@ -2,12 +2,95 @@ package com.java.kangnam.jex3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public static final String TAG = "Test";
+
+    EditText etxtCalc;
+    String[] buffer;
+    int inputCounter;
+    int type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.activity_main6);
+
+        etxtCalc = (EditText)findViewById(R.id.etxtCalc);
+
+        buffer = new String[2];
+        buffer[0] = "";
+        buffer[1] = "";
+        inputCounter = 0;
+        type = 0;
+    }
+
+    public void onClick(View view) {
+        Button pressedButton = ((Button)view);
+        Log.d(TAG, "pressed: " + pressedButton.getText().toString());
+
+        String pressBtnText = pressedButton.getText().toString();
+
+        switch (pressBtnText) {
+            case "Clear":
+                etxtCalc.setText("");
+                type = 0;
+                inputCounter = 0;
+                buffer[0] = "";
+                buffer[1] = "";
+                break;
+            case "OK":
+                Log.d(TAG, "buffer 0 : " + buffer[0] + " buffer 1 : " + buffer[1]);
+                double a = Double.parseDouble(buffer[0]);
+                double b = Double.parseDouble(buffer[1]);
+                double result = 0;
+                if(type == 0) {
+                    result = a + b;
+                }
+                else if(type == 1) {
+                    result = a - b;
+                }
+                else if(type == 2) {
+                    result = a * b;
+                }
+                else if(type == 3) {
+                    result = a / b;
+                }
+                etxtCalc.setText("" + result);
+                type = 0;
+                inputCounter = 0;
+                buffer[0] = "";
+                buffer[1] = "";
+                break;
+            case "+":
+                inputCounter += 1;
+                etxtCalc.append(pressBtnText);
+                type = 0;
+                break;
+            case "-":
+                inputCounter += 1;
+                etxtCalc.append(pressBtnText);
+                type = 1;
+                break;
+            case "*":
+                inputCounter += 1;
+                etxtCalc.append(pressBtnText);
+                type = 2;
+                break;
+            case "/":
+                inputCounter += 1;
+                etxtCalc.append(pressBtnText);
+                type = 3;
+                break;
+            default:
+                buffer[inputCounter] += pressBtnText;
+                etxtCalc.append(pressBtnText);
+                break;
+        }
     }
 }
