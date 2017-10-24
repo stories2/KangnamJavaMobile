509efd6f217d706b265914feee930d2aa67fb74c
Kim HyunWoo <stories2@naver.com>
2017-10-24 16:39:38 +0900

    JEX6 module : make new sample tab

--- JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
+++ JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.java.jex6;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TabHost;
 import android.widget.ViewFlipper;
@@ -15,15 +17,58 @@ public class MainActivity extends Activity {
     MultiAutoCompleteTextView multiAutoTextView;
     ViewFlipper viewFilpImage;
     TabHost tabHost;
+    WebView webViewClient;
+    EditText etxtSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main5);
 
         //Init1();
         //Init2();
-        Init3();
+        //Init3();
+        //Init4();
+        Init5();
+    }
+
+    public void Init5() {
+        tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabImg1 = tabHost.newTabSpec("img1");
+        tabImg1.setContent(R.id.img1);
+        tabImg1.setIndicator("강아지");
+
+        TabHost.TabSpec tabImg2 = tabHost.newTabSpec("img2");
+        tabImg2.setContent(R.id.img2);
+        tabImg2.setIndicator("고양이");
+
+        TabHost.TabSpec tabImg3 = tabHost.newTabSpec("img3");
+        tabImg3.setContent(R.id.img3);
+        tabImg3.setIndicator("토끼");
+
+        TabHost.TabSpec tabImg4 = tabHost.newTabSpec("img4");
+        tabImg4.setContent(R.id.img4);
+        tabImg4.setIndicator("말");
+
+        tabHost.addTab(tabImg1);
+        tabHost.addTab(tabImg2);
+        tabHost.addTab(tabImg3);
+        tabHost.addTab(tabImg4);
+    }
+
+    public void Init4() {
+        etxtSearch = (EditText)findViewById(R.id.etxtSearch);
+        webViewClient = (WebView)findViewById(R.id.webViewClient);
+    }
+
+    public void OnBtnMove(View view) {
+
+    }
+
+    public void OnBtnPrevious(View view) {
+
     }
 
     public void Init3() {
