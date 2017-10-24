0ad0b45e216637f4a93f8655dbcb5a41d12e2263
Kim HyunWoo <stories2@naver.com>
2017-10-24 15:05:20 +0900

    Sample of auto, multi complete edittext

--- JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
+++ JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.java.jex6;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends Activity {
+
+    AutoCompleteTextView autoTextView;
+    MultiAutoCompleteTextView multiAutoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autoTextView = (AutoCompleteTextView) findViewById(R.id.autoTextView);
+        multiAutoTextView = (MultiAutoCompleteTextView)findViewById(R.id.multiAutoTextView);
+
+        String[] items = new String[]{"abc", "abcd", "abcde"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        autoTextView.setAdapter(adapter);
+
+        MultiAutoCompleteTextView.CommaTokenizer commaTokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
+        multiAutoTextView.setTokenizer(commaTokenizer);
+        multiAutoTextView.setAdapter(adapter);
     }
 }
