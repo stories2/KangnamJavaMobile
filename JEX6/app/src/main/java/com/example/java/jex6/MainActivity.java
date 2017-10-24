10279ed64117aced030d3b059448c389eed21cfa
Kim HyunWoo <stories2@naver.com>
2017-10-24 15:32:19 +0900

    EX6 module : filp view sample added

--- JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
+++ JEX6/app/src/main/java/com/example/java/jex6/MainActivity.java
@@ -2,21 +2,38 @@ package com.example.java.jex6;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
 
     AutoCompleteTextView autoTextView;
     MultiAutoCompleteTextView multiAutoTextView;
+    ViewFlipper viewFilpImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        Init1();
+        //Init1();
+        Init2();
+    }
+
+    public void OnBtnStartAutoSwap(View view) {
+        viewFilpImage.startFlipping();
+    }
+
+    public void OnBtnStopAutoSwap(View view) {
+        viewFilpImage.stopFlipping();
+    }
+
+    public void Init2() {
+        viewFilpImage = (ViewFlipper)findViewById(R.id.viewFilpImage);
+        viewFilpImage.setFlipInterval(1000);
     }
 
     public void Init1() {
