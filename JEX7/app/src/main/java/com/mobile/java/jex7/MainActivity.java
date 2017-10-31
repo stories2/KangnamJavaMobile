3e77d1c69c40aa4943cd63e57fd3eb56e3a09962
Kim HyunWoo <stories2@naver.com>
2017-10-31 15:02:46 +0900

    background color change module : if option click then the background color will change

--- JEX7/app/src/main/java/com/mobile/java/jex7/MainActivity.java
+++ JEX7/app/src/main/java/com/mobile/java/jex7/MainActivity.java
@@ -1,16 +1,22 @@
 package com.mobile.java.jex7;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+
+    LinearLayout linearMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearMain = (LinearLayout)findViewById(R.id.linearMain);
     }
 
     @Override
@@ -28,10 +34,13 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.itemRed:
+                linearMain.setBackgroundColor(Color.RED);
                 break;
             case R.id.itemGreen:
+                linearMain.setBackgroundColor(Color.GREEN);
                 break;
             case R.id.itemBlue:
+                linearMain.setBackgroundColor(Color.BLUE);
                 break;
             case R.id.itemHello:
                 break;
