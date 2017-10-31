50c4136f4976870d0ca70ba8eb2ba4669fa28e4f
Kim HyunWoo <stories2@naver.com>
2017-10-31 15:00:27 +0900

    option module : make new option menus

--- JEX7/app/src/main/java/com/mobile/java/jex7/MainActivity.java
+++ JEX7/app/src/main/java/com/mobile/java/jex7/MainActivity.java
@@ -2,6 +2,8 @@ package com.mobile.java.jex7;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemRed:
+                break;
+            case R.id.itemGreen:
+                break;
+            case R.id.itemBlue:
+                break;
+            case R.id.itemHello:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
