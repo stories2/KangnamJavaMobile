99b54d382fb990c46d0469b7ccfb764f4357d8b3
stories2 <stories2@naver.com>
2017-11-12 20:57:14 +0900

    getting selection event

--- JEX10/app/src/main/java/com/mobile/jex10/MainActivity.java
+++ JEX10/app/src/main/java/com/mobile/jex10/MainActivity.java
@@ -2,12 +2,41 @@ package com.mobile.jex10;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
+
+    RadioGroup radioGroupAnimal;
+    final String TAG = "JEX10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroupAnimal = (RadioGroup) findViewById(R.id.radioGroupAnimal);
+    }
+
+    public void BtnOnPictureShowClick(View view) {
+
+        switch (radioGroupAnimal.getCheckedRadioButtonId()) {
+            case R.id.radioDog:
+                Log.d(TAG, "picture show: dog");
+                break;
+            case R.id.radioCat:
+                Log.d(TAG, "picture show: cat");
+                break;
+            case R.id.radioRabbit:
+                Log.d(TAG, "picture show: rabbit");
+                break;
+            case R.id.radioHorse:
+                Log.d(TAG, "picture show: horse");
+                break;
+            default:
+                Log.d(TAG, "picture show: default");
+                break;
+        }
     }
 }
