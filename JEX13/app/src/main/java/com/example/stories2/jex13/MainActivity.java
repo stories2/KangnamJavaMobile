4495da1b8005fc16485c388648af8f45775fbed7
Kim HyunWoo <stories2@naver.com>
2017-11-28 15:20:59 +0900

    multiple activity

--- JEX13/app/src/main/java/com/example/stories2/jex13/MainActivity.java
+++ JEX13/app/src/main/java/com/example/stories2/jex13/MainActivity.java
@@ -4,17 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
+
+    RadioGroup radioGroupActivity;
+    int activityTarget = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroupActivity = (RadioGroup)findViewById(R.id.radioGroupActivity);
+
+        radioGroupActivity.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId) {
+                    case R.id.radioSecond:
+                        break;
+                    case R.id.radioThird:
+                        break;
+                }
+            }
+        });
     }
 
     public void btnGoActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        startActivity(intent);
+        if(activityTarget == 0) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else if(activityTarget == 1) {
+            Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+            startActivity(intent);
+        }
     }
 }
