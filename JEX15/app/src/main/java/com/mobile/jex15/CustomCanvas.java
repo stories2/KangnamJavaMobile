64b32b4f01a2f2f7f34af423e2d77cb8c543b969
stories2 <stories2@naver.com>
2017-12-02 01:42:00 +0900

    solve homework

--- JEX15/app/src/main/java/com/mobile/jex15/CustomCanvas.java
+++ JEX15/app/src/main/java/com/mobile/jex15/CustomCanvas.java
@@ -2,11 +2,20 @@ package com.mobile.jex15;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mobile.jex15.MainActivity.COLOR_BLUE;
+import static com.mobile.jex15.MainActivity.COLOR_GREEN;
+import static com.mobile.jex15.MainActivity.COLOR_RED;
+import static com.mobile.jex15.MainActivity.DRAW_CIRCLE;
+import static com.mobile.jex15.MainActivity.DRAW_LINE;
+import static com.mobile.jex15.MainActivity.DRAW_RECTANGLE;
 
 /**
  * Created by stories2 on 2017. 12. 1..
@@ -29,6 +38,36 @@ public class CustomCanvas extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+
+        for(MyShape myShape : myShapes) {
+            switch (myShape.getColor()) {
+                case COLOR_RED:
+                    paint.setColor(Color.RED);
+                    break;
+                case COLOR_GREEN:
+                    paint.setColor(Color.GREEN);
+                    break;
+                case COLOR_BLUE:
+                    paint.setColor(Color.BLUE);
+                    break;
+            }
+            switch (myShape.getShapeType()) {
+                case DRAW_LINE:
+                    canvas.drawLine(myShape.getStartX(), myShape.getStartY(), myShape.getStopX(), myShape.getStopY(), paint);
+                    break;
+                case DRAW_CIRCLE:
+                    int radius = (int)Math.sqrt(Math.pow(myShape.getStartX() - myShape.getStopX(), 2) + Math.pow(myShape.getStartY() - myShape.getStopY(), 2));
+                    canvas.drawCircle(myShape.getStartX(), myShape.getStartY(), radius, paint);
+                    break;
+                case DRAW_RECTANGLE:
+                    canvas.drawRect(myShape.getStartX(), myShape.getStartY(), myShape.getStopX(), myShape.getStopY(), paint);
+                    break;
+            }
+        }
     }
 
     public void AppendShape(MyShape myShape) {
--- JEX15/app/src/main/java/com/mobile/jex15/MainActivity.java
+++ JEX15/app/src/main/java/com/mobile/jex15/MainActivity.java
@@ -2,11 +2,21 @@ package com.mobile.jex15;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.SubMenu;
 
 public class MainActivity extends Activity {
 
+    public static final int
+            DRAW_LINE = 1, DRAW_CIRCLE = 2, DRAW_RECTANGLE = 3,
+            COLOR_RED = 4, COLOR_GREEN = 5, COLOR_BLUE = 6;
+
     CustomCanvas customCanvas;
+    int lastDrawStyle = DRAW_LINE, lastDrawColor = COLOR_RED;
+    MyShape myShape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +28,11 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        MyShape myShape = new MyShape();
         super.onTouchEvent(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                myShape = new MyShape();
+
                 myShape.setStartX((int) event.getX());
                 myShape.setStartY((int) event.getY());
                 break;
@@ -30,7 +41,56 @@ public class MainActivity extends Activity {
             case MotionEvent.ACTION_UP:
                 myShape.setStopX((int) event.getX());
                 myShape.setStopY((int) event.getY());
+                myShape.setColor(lastDrawColor);
+                myShape.setShapeType(lastDrawStyle);
                 customCanvas.AppendShape(myShape);
+
+                Log.d("JEX15", "new draw data: " +
+                myShape.getStartX() + ", " + myShape.getStartY() +
+                " " + myShape.getStopX() + ", " + myShape.getStopY() +
+                " c: " + myShape.getColor() + " s: " + myShape.getShapeType());
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, DRAW_LINE, 0, "Draw line");
+        menu.add(0, DRAW_CIRCLE, 0, "Draw circle");
+        menu.add(0, DRAW_RECTANGLE, 0, "Draw rectangle");
+
+        SubMenu subMenu = menu.addSubMenu("Change color");
+        subMenu.add(0, COLOR_RED, 0, "Red");
+        subMenu.add(0, COLOR_GREEN, 0, "Green");
+        subMenu.add(0, COLOR_BLUE, 0, "Blue");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case DRAW_LINE:
+                lastDrawStyle = DRAW_LINE;
+                break;
+            case DRAW_CIRCLE:
+                lastDrawStyle = DRAW_CIRCLE;
+                break;
+            case DRAW_RECTANGLE:
+                lastDrawStyle = DRAW_RECTANGLE;
+                break;
+            case COLOR_RED:
+                lastDrawColor = COLOR_RED;
+                break;
+            case COLOR_GREEN:
+                lastDrawColor = COLOR_GREEN;
+                break;
+            case COLOR_BLUE:
+                lastDrawColor = COLOR_BLUE;
                 break;
         }
         return true;
