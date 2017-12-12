2a79e456731c7db372ffaa8eae760e3ac8fa3c47
stories2 <stories2@naver.com>
2017-12-12 14:58:58 +0900

    listview sample

--- JEX17/app/src/main/java/com/examplejex17/user/jex17/MainActivity.java
+++ JEX17/app/src/main/java/com/examplejex17/user/jex17/MainActivity.java
@@ -2,12 +2,38 @@ package com.examplejex17.user.jex17;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init1();
+    }
+
+    public void Init1() {
+        listView = (ListView)findViewById(R.id.listView);
+
+        final String[] items = {"a" ,"b", "c", "d", "e"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), items[i], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
